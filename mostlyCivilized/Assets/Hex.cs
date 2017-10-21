cdb81f75d40467804256df6ab9db3df561f27b9a
Jurmungandur <31280587+Jurmungandur@users.noreply.github.com>
2017-10-21 18:51:58 +0200

    HexEngine v1.0
    Making a simple hexagon map generator.

---
+++ mostlyCivilized/Assets/Hex.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex {
+
+    public Hex(int q, int r)
+    {
+        this.Q = q;
+        this.R = r;
+        this.R = -(q + r);
+    }
+
+    public readonly int Q;
+    public readonly int R;
+    public readonly int S;
+
+    static readonly float WIDTH_MULTIPLIER = Mathf.Sqrt(3) / 2;
+
+    public Vector3 Pos()
+    {
+        float radius = 1;
+        float height = radius * 2;
+        float width =  WIDTH_MULTIPLIER * height;
+
+        float horiz = width;
+        float vert = height * 0.75f;
+
+        return new Vector3(horiz * (this.Q + this.R / 2f),0,vert * this.R);
+    }
+}
---
+++ mostlyCivilized/Assets/HexMap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap : MonoBehaviour {
+
+	void Start ()
+    {
+        GenerateMap();
+    }
+
+    public GameObject hexPrefab;
+
+    public void GenerateMap()
+    {
+        for (int column = 0; column < 10; column++)
+        {
+            for (int row = 0; row < 10; row++)
+            {
+                //Instantiate a Hex
+                Hex h = new Hex(column, row);
+
+                Instantiate(hexPrefab, -h.Pos(), Quaternion.identity, this.transform);
+            }
+        }
+    }
+}
